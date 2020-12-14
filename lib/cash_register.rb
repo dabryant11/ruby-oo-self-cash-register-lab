0d44cadc9873b1f3fbfbef4234a1eedbc61ed1cc
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2020-12-13 17:56:24 -0800

    commit tree cc6e2d9cbdc10aa80334f6aeb63c34942f4aa262

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
+class CashRegister 
 
+    attr_reader 
+    attr_accessor :total, :discount
+
+    @@items = [] 
+
+    def initialize (total_par = 0, employee_discount = .20 )
+        @total = total_par
+        @discount = employee_discount
+        @@items << self
+    end 
+
+    def total 
+    end
+
+    def add_item 
+    end
+
+    def apply_discount (total_par,employee_discount)
+        new_total = total_par * employee_discount
+        new_total
+    end 
+
+    def items 
+        @@items
+    end 
+
+    def void_last_transaction 
+    end
+
+end 
+
+customer1 = CashRegister.new(2)
+# binding.pry
